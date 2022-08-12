cca9fe9a0774db6282ccefad9ac8d9c261e90344
Anna <green.abrikos.73@gmail.com>
2022-08-12 12:12:26 +0300

    The task is ready

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
-module.exports = function reverse (n) {
-  
+module.exports = 
+function reverse (n) {
+
+let arr = Array.from(n.toString());
+arr.reverse();
+return arr.slice(0, 3).join('');
+
 }
+
